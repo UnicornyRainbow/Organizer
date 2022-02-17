3d713ed8bcaae85918ebefaf6d7f77f0cacfb426
blackmoonboy <blackforlive@protonmail.com>
2022-02-17 21:38:23 +0100

    Tickets can now be loaded from files and newly created Tickets are automatically displayed

--- Organizer.py
+++ Organizer.py
@@ -12,17 +12,21 @@ from gi.repository import Gtk, Adw
 class Ticket(Gtk.ApplicationWindow):
 	def __init__(self, title, topic, effort, priority):
 		self.frame = Gtk.Frame()
-		self.box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 0)
+		self.box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 2)
+		self.box.set_margin_top(3)
 		
 		self.titleBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		self.titleBox.set_margin_start(3)
 		self.title = Gtk.Label()
 		self.title.set_label(title)
 		self.titleBox.append(self.title)
 		self.topicBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		self.topicBox.set_margin_start(3)
 		self.topic = Gtk.Label()
 		self.topic.set_label(topic)
 		self.topicBox.append(self.topic)
 		self.valBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL,homogeneous = True)
+		self.valBox.set_margin_start(3)
 		self.effort = Gtk.Label()
 		self.effort.set_label(effort)
 		self.effortBox = Gtk.Box()
@@ -51,20 +55,15 @@ class Ticket(Gtk.ApplicationWindow):
 		self.box.append(self.statusBox)
 		self.frame.set_child(self.box)
 		
-		#window.idea.add(self.box)
 		
 	def moveTo(self, widget):
 		file = widget.get_active_text()
-		if file == 'Idea':
-			pass
-		elif file == 'To Do':
-			pass
-		elif file == 'In Progress':
-			pass
-		elif file == 'On Hold/Waiting':
-			pass
-		elif file == 'Done':
-			pass
+		#if file not 'Done':
+		#	print('not Done')
+			
+		
+		#elif file == 'Done':
+		#	pass
 		
 	def getFile(self):
 		pass
@@ -77,19 +76,20 @@ class Ticket(Gtk.ApplicationWindow):
 		
 	def setComment(self):
 		pass
+		
 
 
 class app():
 	
 	#creates a new Ticket
-	def createTicket(title, topic, effort, priority, description):
-		content = {'Title': title, 'Topic': topic, 'Effort': effort, 'Priority': priority, 'Description': description}
+	def createTicket(title='', topic='', effort='', priority='', description=''):
+		print(title+topic+effort+priority+description)
+		content = {'Title': title, 'Topic': topic, 'Effort': effort, 'Priority': priority, 'Description': description, 'Position': 'Idea'}
 		id = str(datetime.datetime.now()).replace(' ', '_').replace('.', '_') + '.ticket'
 		path = app.readConfig('Location of Tickets') + '/'
 		with open(path + id, 'w') as ticket:
 			for entry in content:
 				ticket.write(entry + ': ' + content[entry] + '\n')
-		#app.refreshTickets()
 		
 	def refreshTickets():
 		fileList = app.getTickets()
@@ -104,9 +104,18 @@ class app():
 				if entry.is_file():					#hides folders
 					if entry.name.endswith('.ticket') and not entry.name.startswith('.'):
 						fileList.append(entry.name)
-			
-		print(fileList)
-		#return fileList
+		return fileList
+	
+	#reads the contents in a ticket file
+	def getTicketContent(file):
+		dict={}
+		path = app.readConfig('Location of Tickets')
+		with open(path+'/'+file) as ticket:
+			for line in ticket:
+				line = line.strip('\n').split(': ')
+				dict[line[0]] = line[1]
+		return dict
+		
 		
 	#writes the config
 	def setConfig(setting, content):
@@ -114,7 +123,7 @@ class app():
 		for settings in allSettings:
 			if settings[0] == setting:
 				settings[1] = content
-		with open('board.config', 'w') as config:
+		with open(os.path.dirname(__file__)+'/Organizer.config', 'w') as config:
 			for settings in allSettings:
 				config.write(': '.join(settings) + '\n')
 			
@@ -122,7 +131,7 @@ class app():
 	#reads the config
 	def readConfig(setting):
 		allSettings = []
-		with open('board.config', 'r') as config:
+		with open(os.path.dirname(__file__)+'/Organizer.config', 'r') as config:
 			for line in config:
 				line = line.strip().split(': ')
 				if setting == 'allSettings':
@@ -133,7 +142,7 @@ class app():
 
 class window(Gtk.ApplicationWindow):
 	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)#'Codey', 960, 540, **kwargs)
+		super().__init__(*args, **kwargs)
 		
 		app3 = self.get_application()
 		sm = app3.get_style_manager()
@@ -171,7 +180,7 @@ class window(Gtk.ApplicationWindow):
 		
 		#Window For Ticket Details
 		self.detailBox = Gtk.Box()
-		self.mainBox.prepend(self.detailBox)
+		#self.mainBox.append(self.detailBox)
 		
 
 
@@ -183,6 +192,12 @@ class window(Gtk.ApplicationWindow):
 		self.newButton.set_child(self.newIcon)
 		self.newButton.connect('clicked', self.newTicket)
 		self.headerBar.pack_start(self.newButton)
+
+		#Button to test functions
+		self.testButton = Gtk.Button()
+		self.testButton.set_label('test')
+		self.testButton.connect('clicked', self.reloadTickets)
+		self.headerBar.pack_start(self.testButton)
 
 		#Hamburger Menu
 		#Popover and Button
@@ -207,19 +222,19 @@ class window(Gtk.ApplicationWindow):
 		self.ticketBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 10)
 		self.scrolledWindow.set_child(self.ticketBox)
 		self.idea = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 10)
-		self.idea.set_size_request(143, -1)
+		self.idea.set_size_request(160, -1)
 		self.ticketBox.append(self.idea)
 		self.todo = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 10)
-		self.todo.set_size_request(143, -1)
+		self.todo.set_size_request(160, -1)
 		self.ticketBox.append(self.todo)
 		self.inProgress = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 10)
-		self.inProgress.set_size_request(143, -1)
+		self.inProgress.set_size_request(160, -1)
 		self.ticketBox.append(self.inProgress)
 		self.onHold = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 10)
-		self.onHold.set_size_request(143, -1)
+		self.onHold.set_size_request(160, -1)
 		self.ticketBox.append(self.onHold)
 		self.done = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 10)
-		self.done.set_size_request(143, -1)
+		self.done.set_size_request(160, -1)
 		self.ticketBox.append(self.done)
 		
 		#Title of Ticket Categories
@@ -235,18 +250,19 @@ class window(Gtk.ApplicationWindow):
 		self.doneLabel.set_markup('<big><b>Done</b></big>')
 		
 		self.titleBox.append(self.ideaLabel)
-		self.ideaLabel.set_size_request(143, -1)
+		self.ideaLabel.set_size_request(160, -1)
 		self.titleBox.append(self.todoLabel)
-		self.todoLabel.set_size_request(143, -1)
+		self.todoLabel.set_size_request(160, -1)
 		self.titleBox.append(self.inProgressLabel)
-		self.inProgressLabel.set_size_request(143, -1)
+		self.inProgressLabel.set_size_request(160, -1)
 		self.titleBox.append(self.onHoldLabel)
-		self.onHoldLabel.set_size_request(143, -1)
+		self.onHoldLabel.set_size_request(160, -1)
 		self.titleBox.append(self.doneLabel)
-		self.doneLabel.set_size_request(143, -1)
+		self.doneLabel.set_size_request(160, -1)
 		
-		self.newticket = Ticket('title', 'topic', '10', '1')
-		self.todo.append(self.newticket.frame)
+		
+		#finally gets loads the Tickets for the first time
+		self.getTickets()
 		
 		
 	#opens dialog to choose folder to look in
@@ -256,21 +272,51 @@ class window(Gtk.ApplicationWindow):
 		dialog.add_buttons('Cancel', Gtk.ResponseType.CANCEL, 'Open', Gtk.ResponseType.OK)
 		dialog.connect('response', self.on_dialog_response)
 		dialog.show()
-		
 	def on_dialog_response(self, widget, response_id):
 		if response_id == Gtk.ResponseType.OK:
 			app.setConfig('Location of Tickets', widget.get_file().get_path())
 		widget.destroy()
 		
 		
+	def getTickets(self):
+		files = app.getTickets()
+		for file in files:
+			content = app.getTicketContent(file)
+			self.newticket = Ticket(content['Title'], content['Topic'], content['Effort'], content['Priority'])
+			if content['Position'] == 'Idea':
+				self.idea.append(self.newticket.frame)
+			if content['Position'] == 'To Do':
+				self.todo.append(self.newticket.frame)
+			if content['Position'] == 'In Progress':
+				self.inProgress.append(self.newticket.frame)
+			if content['Position'] == 'On Hold/Waiting':
+				self.onHold.append(self.newticket.frame)
+			if content['Position'] == 'Done':
+				self.done.append(self.newticket.frame)
+			
+			
+	def reloadTickets(self):
+		while self.idea.get_first_child():
+			self.idea.remove(self.idea.get_first_child())
+		while self.todo.get_first_child():
+			self.todo.remove(self.todo.get_first_child())
+		while self.inProgress.get_first_child():
+			self.inProgress.remove(self.inProgress.get_first_child())
+		while self.onHold.get_first_child():
+			self.onHold.remove(self.onHold.get_first_child())
+		while self.done.get_first_child():
+			self.done.remove(self.done.get_first_child())
+		self.getTickets()
+	
+		
 	#creates new Ticket
 	def newTicket(self, widget):
 		dialog = ticketDialog(self)
 		dialog.connect('response', self.on_ticket_response)
-		
 	def on_ticket_response(self, widget, response_id):
 		if response_id == Gtk.ResponseType.OK:
 			app.createTicket(widget.title.get_text(), widget.topic.get_text(), widget.effort.get_text(), widget.priority.get_text(), widget.description.get_text())
+			self.reloadTickets()
 		widget.destroy()
 		
 class ticketDialog(Gtk.Dialog):
@@ -299,14 +345,11 @@ class ticketDialog(Gtk.Dialog):
 		self.contentBox.append(self.description)
 		self.show()
 		
-		
+	
 	#move ticket with dropdown
-	#save and load tickets, add ticket id(maybe show id in frame?)
 	#click Ticket to open details in side window
-	#add description(entered in when created)
 	#add comments
 	#setting for topic and choose them via dropdown
-	#setting to choose where tickets saved
 
 class MyApp(Adw.Application):
 	def __init__(self, **kwargs):
@@ -318,9 +361,6 @@ class MyApp(Adw.Application):
 		self.win.present()
 	
 
-#window = window()
-#window.connect('delete-event', Gtk.main_quit)
-#window.show_all()
-#Gtk.main()
+
 app2=MyApp(application_id='org.Unicorn.Organizer')
 app2.run(sys.argv)
