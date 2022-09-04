f8bb9a88ad9810e22b2d72669151d2d1034a3b3e
UnicornyRainbow <unicorn@fastmail.com>
2022-09-04 16:28:55 +0200

    fixed .gitignore, added debug mode

--- flatpak_build.sh
+++ flatpak_build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-
-echo "building package...................."
-flatpak-builder build io.github.unicornyrainbow.organizer.yml --force-clean
-echo "making .flatpak file..............."
+clear
 rm -rf repo
+echo "##########################  building  package  ##########################"
 flatpak-builder --force-clean --repo=repo build io.github.unicornyrainbow.organizer.yml	
 flatpak build-bundle repo organizer.flatpak io.github.unicornyrainbow.organizer
-echo "installing........................"
+echo "#########################  deleting old pakage  #########################"
 flatpak remove --force-remove --delete-data --noninteractive -y organizer
+echo "########################  installing new pakage  ########################"
 flatpak-builder --user --install --force-clean build "io.github.unicornyrainbow.organizer.yml"
